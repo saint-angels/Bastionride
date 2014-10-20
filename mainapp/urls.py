d8df26080eb85c96026bbf2b41150467bf2d08a8
Mike Prokofyev <mikeprokofyev@gmail.com>
2014-10-20 17:33:25 +0600

    index.html, about.html and css for them created

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('mainapp.views',
+
     url(r'^$', 'index', name='index'),
+    url(r'^about/$', 'about', name='about'),
 )
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main_site/index.html', {})
+
+
+def about(request):
+    return render(request, 'main_site/about.html', {})
