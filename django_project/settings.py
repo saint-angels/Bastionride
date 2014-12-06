767ab4432b07538bc2cf96b8e5a4ac0fec490074
Mike Prokofyev <mikeprokofyev@gmail.com>
2014-12-06 11:52:24 +0500

    settings update

--- django_project/settings.py
+++ django_project/settings.py
@@ -60,12 +60,8 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'CoUIOWS33M',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
